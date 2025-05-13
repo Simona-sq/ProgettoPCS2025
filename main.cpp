2375c61400909a79b7a713a1545a6c2a6c5d32b3
Simona Squeglia <simonasqueglia@MacBook-Air-di-simona.local>
2025-05-13 14:12:24 +0200

    update

--- main.cpp
+++ main.cpp
@@ -7,29 +7,46 @@ using namespace std;
 using namespace Eigen;
 using namespace PolyhedronLibrary;
 
-int main()
+int main(int argc, char* argv[])
 {
-	unsigned int p;
-	unsigned int q;
-	unsigned int b;
-	unsigned int c;
-	unsigned int v1;
-	unsigned int v2;
-	cout << "Inserisci la quadrupla/sestupla del poliedro nel formato p q b c v1 v2 (lasciare vuoto nel caso non si vogliano inserire i valori di v1 e v2):\n";
-	//cin >> p >> q >> b >> c;
-
+	unsigned int p = 0;
+	unsigned int q = 0;
+	unsigned int b = 0;
+	unsigned int c = 0;
+	unsigned int v1 = 0;
+	unsigned int v2 = 0;
+	
 	// controllo che il tipo dei valori inseriti sia int
-	if (!(std::cin >> p >> q >> b >> c >> v1 >> v2)) {
-        cerr << "Errore: inserire 4 numeri interi positivi.\n";
-        return 1;
+	cout << argc << endl;
+	if (argc == 5) {
+		p = stoi(argv[1]);
+		q = atoi(argv[2]);
+		b = atoi(argv[3]);
+		c = atoi(argv[4]);
+        cout << "ok" << p << q << b << c << endl;
     }
+	else if (argc == 7) {
+		p = atoi(argv[1]);
+		q = atoi(argv[2]);
+		b = atoi(argv[3]);
+		c = atoi(argv[4]);
+		v1 = atoi(argv[5]);
+		v2 = atoi(argv[6]);
+        cout << "ok" << p << q << b << c << v1 << v2 << endl;
+    }
+	else
+ 	{
+		cerr << "Inserimento non valido. Inserire 4 o 6 numeri interi positivi.\n" <<endl;
+		return 1;
+	}
 
 	// controllo le condizioni su p e q
-	if (p < 3 || q <3){
+	cout << p << " " << q << " "<< double (1.0/p) <<endl;
+	if (p < 3 || q < 3 || double (1.0/p)+double (1.0/q) <= 0.5){
 		cerr << "Errore: i parametri p e q non rispettano le condizioni dei poligoni platonici.\n";
-        return 1;
-
+    	return 1;
 	}
+	
 
 	// controllo la disugualianza dei poligoni platonici
 
